f95d47f60cfbf77c6b3f664eca8ae30aa0194db6
SidharthReddy11265 <pandiri.s@northeastern.edu>
2022-09-21 01:21:28 -0400

    Assignment-1_INFO5100

---
+++ src/User/User.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+import java.io.File;
+/**
+ *
+ * @author psidh
+ */
+public class User {
+
+ 
+    private String First_Name;
+    private String Last_Name;
+    private String DOB;
+    private String Age;
+    private String Telephone_number;
+    private String Email;
+    private String Street_Line_1;
+    private String Street_Line_2;
+    private String City;
+    private String Country;
+    private String Affiliation;
+    private String Major;
+    private String Career;
+    private String Degree_1;
+    private String Degree_1_Start;
+    private String Degree_1_End;
+    private String Degree_2;
+    private String Degree_2_Start;
+    private String Degree_2_End;
+    private File Photo;
+    
+    public File getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(File Photo) {
+        this.Photo = Photo;
+    }
+
+    public String getFirst_Name() {
+        return First_Name;
+    }
+
+    public void setFirst_Name(String First_Name) {
+        this.First_Name = First_Name;
+    }
+
+    public String getLast_Name() {
+        return Last_Name;
+    }
+
+    public void setLast_Name(String Last_Name) {
+        this.Last_Name = Last_Name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getTelephone_number() {
+        return Telephone_number;
+    }
+
+    public void setTelephone_number(String Telephone_number) {
+        this.Telephone_number = Telephone_number;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStreet_Line_1() {
+        return Street_Line_1;
+    }
+
+    public void setStreet_Line_1(String Street_Line_1) {
+        this.Street_Line_1 = Street_Line_1;
+    }
+
+    public String getStreet_Line_2() {
+        return Street_Line_2;
+    }
+
+    public void setStreet_Line_2(String Street_Line_2) {
+        this.Street_Line_2 = Street_Line_2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public String getDegree_1() {
+        return Degree_1;
+    }
+
+    public void setDegree_1(String Degree_1) {
+        this.Degree_1 = Degree_1;
+    }
+
+    public String getDegree_1_Start() {
+        return Degree_1_Start;
+    }
+
+    public void setDegree_1_Start(String Degree_1_Start) {
+        this.Degree_1_Start = Degree_1_Start;
+    }
+
+    public String getDegree_1_End() {
+        return Degree_1_End;
+    }
+
+    public void setDegree_1_End(String Degree_1_End) {
+        this.Degree_1_End = Degree_1_End;
+    }
+
+    public String getDegree_2() {
+        return Degree_2;
+    }
+
+    public void setDegree_2(String Degree_2) {
+        this.Degree_2 = Degree_2;
+    }
+
+    public String getDegree_2_Start() {
+        return Degree_2_Start;
+    }
+
+    public void setDegree_2_Start(String Degree_2_Start) {
+        this.Degree_2_Start = Degree_2_Start;
+    }
+
+    public String getDegree_2_End() {
+        return Degree_2_End;
+    }
+
+    public void setDegree_2_End(String Degree_2_End) {
+        this.Degree_2_End = Degree_2_End;
+    }
+
+
+}
+
